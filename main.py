b510020316184f07a7baa750e7b892d2d82fd13c
RyanMontville <ryan.a.montville@gmail.com>
2025-02-21 16:24:30 -0600

    created method to generate the mosaic grid

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from PIL import Image
 from letters import drawA
+
 rows = []
 
-def generate_grid(background_color):
-    pass
+
+def generate_grid(background_color, width, height):
+    mosaic_pixel_width = width * 30
+    mosaic_pixel_height = height * 30
+    grid = []
+    for y in range(mosaic_pixel_height):
+        row = []
+        for x in range(mosaic_pixel_width):
+            row.append(background_color)
+        grid.append(row)
+
+    return grid
 
 
+# rows = generate_grid("red", 10, 10)
 current_x = 1
 current_y = 1
-# rows = drawA(rows, current_x, current_y, "white", "red")
+rows = drawA(rows, current_x, current_y, "white", "red")
 
 print(rows)
 
